8679d4b737fca927d30c0e66b98a5be42d8230c9
Reza <gr8coder.com@gmail.com>
2023-04-23 10:37:47 -0400

    first

---
+++ q1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    cout<<"Please enter a number: ";
+    cin>>num;
+
+    if(num==1){
+        cout<<"You have entered 1"<<endl;
+    }
+    else{
+        if(num==2 || num==3){
+            cout<<"You have entered 2 or 3"<<endl;
+        }
+        else{
+            cout<<"You have not entered 1 or 2 or 3"<<endl;
+        }
+    }
+}
